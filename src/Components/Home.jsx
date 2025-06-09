4d18a54d3a7114fa43f9a079d6d7373ee59bfede
Thilip-G <thiliptsdeveloper@gmail.com>
2025-06-09 16:47:31 +0530

    Updated resums add

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -85,7 +85,7 @@ const Home = () => {
                     </motion.div>
 
                     <motion.a
-                        href="/Thilip.pdf"
+                        href="/public/Thilip-CV.pdf"
                         download="Thilip.pdf"
                         className="btn btn-primary mb-2"
                         initial={{ opacity: 0, y: 20 }}
